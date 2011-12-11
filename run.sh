5e7642166e7bddbb9710fa63e96c77198c4bee72
Jonas Pfenniger <jonas@pfenniger.name>
2011-12-11 13:04:29 +0000

    Move custom scripts outside of server/

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cd `dirname $0`
+
+rm -f server.stop
+
+while ! [ -f server.stop ]; do
+  if ! [ -p server.ctl ]; then
+    rm -f server.ctl
+    mkfifo server.ctl
+  fi
+
+  git pull
+
+  mkdir -p server
+  pushd server >/dev/null
+    java -Xincgc -Xmx1G -jar ../craftbukkit-1.0.0-SNAPSHOT.jar nogui <> ../server.ctl &>/dev/null
+
+    if [ "$?" != 0 ]; then
+      echo "Fail !" `date` > ../fail.log
+    fi
+  popd >/dev/null
+
+  sleep 1
+done
